7b0c7cc0ef7fdae178a9d5cde1ab188bd75e397e
VoronkovaVera <voronkovaw@gmail.com>
2023-11-24 23:01:55 +0300

    -

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void meow() {
+        System.out.println("Meow");
+    }
+}
